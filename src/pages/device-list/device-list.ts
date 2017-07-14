32b50e6c3956b00d0bcd5870d43e4e0f5f441036
formulahendry <formulahendry@gmail.com>
2017-07-14 15:23:03 +0800

    Add loading spinner

--- src/pages/device-list/device-list.ts
+++ src/pages/device-list/device-list.ts
@@ -10,31 +10,37 @@ import { ConnectionString } from 'azure-iot-device';
   templateUrl: 'device-list.html'
 })
 export class DeviceList {
-  items: Array<{ deviceId: string, deviceConnectionString: string }>;
+  items: Array<{ deviceId: string, deviceConnectionString: string, iotHubConnectionString: string }>;
+  isLoading: boolean = true;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.items = [];
+    let iotHubConnectionString = 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=bl8Ok/bA1infvdDdi8f6XlMmBmT44BrwOkOo7Q+HuZ0=';
     if (Utility.isApp()) {
-      let iotHubConnectionString = 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=bl8Ok/bA1infvdDdi8f6XlMmBmT44BrwOkOo7Q+HuZ0=';
       let registry = iothub.Registry.fromConnectionString(iotHubConnectionString);
       registry.list((err, deviceList) => {
         deviceList.forEach((device, index) => {
           this.items.push({
             deviceId: device.deviceId,
             deviceConnectionString: ConnectionString.createWithSharedAccessKey(Utility.getHostName(iotHubConnectionString),
-                        device.deviceId, device.authentication.SymmetricKey.primaryKey)
+              device.deviceId, device.authentication.SymmetricKey.primaryKey),
+            iotHubConnectionString: iotHubConnectionString
           });
         });
+        this.isLoading = false;
       });
     } else {
       this.items.push({
         deviceId: 'DoorMonitor-Mock',
-        deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DoorMonitor;SharedAccessKey=UiTdG5iMFnMk6dnI9GQzq4UKuqAFui1Mq1kq+doHHVQ='
+        deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DoorMonitor;SharedAccessKey=UiTdG5iMFnMk6dnI9GQzq4UKuqAFui1Mq1kq+doHHVQ=',
+        iotHubConnectionString: iotHubConnectionString
       });
       this.items.push({
         deviceId: 'DevKit-Mock',
-        deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DevKit;SharedAccessKey=6lCf2leXhkD7N2pAUsGMx24LnaxfDJ9t5yMgzNmHutg='
+        deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DevKit;SharedAccessKey=6lCf2leXhkD7N2pAUsGMx24LnaxfDJ9t5yMgzNmHutg=',
+        iotHubConnectionString: iotHubConnectionString
       });
+      setTimeout(() => { this.isLoading = false; }, 2000);
     }
   }
 
