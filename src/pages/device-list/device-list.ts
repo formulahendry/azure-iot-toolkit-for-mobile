4769977267e1224f93467bb5fd1e90ba22fec511
formulahendry <formulahendry@gmail.com>
2017-07-17 13:50:34 +0800

    Add pull-to-refresh

--- src/pages/device-list/device-list.ts
+++ src/pages/device-list/device-list.ts
@@ -10,15 +10,19 @@ import { ConnectionString } from 'azure-iot-device';
   templateUrl: 'device-list.html'
 })
 export class DeviceList {
-  items: Array<{ deviceId: string, deviceConnectionString: string, iotHubConnectionString: string }>;
+  items: Array<{ deviceId: string, deviceConnectionString: string, iotHubConnectionString: string }> = [];
   isLoading: boolean = true;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.items = [];
+    this.listDevices();
+  }
+
+  listDevices(refresher = null) {
     let iotHubConnectionString = 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=bl8Ok/bA1infvdDdi8f6XlMmBmT44BrwOkOo7Q+HuZ0=';
     if (Utility.isApp()) {
       let registry = iothub.Registry.fromConnectionString(iotHubConnectionString);
       registry.list((err, deviceList) => {
+        this.items = [];
         deviceList.forEach((device, index) => {
           this.items.push({
             deviceId: device.deviceId,
@@ -27,20 +31,31 @@ export class DeviceList {
             iotHubConnectionString: iotHubConnectionString
           });
         });
-        this.isLoading = false;
+        this.loadComplete(refresher);
       });
     } else {
-      this.items.push({
-        deviceId: 'DoorMonitor-Mock',
-        deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DoorMonitor;SharedAccessKey=UiTdG5iMFnMk6dnI9GQzq4UKuqAFui1Mq1kq+doHHVQ=',
-        iotHubConnectionString: iotHubConnectionString
-      });
-      this.items.push({
-        deviceId: 'DevKit-Mock',
-        deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DevKit;SharedAccessKey=6lCf2leXhkD7N2pAUsGMx24LnaxfDJ9t5yMgzNmHutg=',
-        iotHubConnectionString: iotHubConnectionString
-      });
-      setTimeout(() => { this.isLoading = false; }, 2000);
+      setTimeout(() => {
+        this.items = [];
+        this.items.push({
+          deviceId: 'DoorMonitor-Mock',
+          deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DoorMonitor;SharedAccessKey=UiTdG5iMFnMk6dnI9GQzq4UKuqAFui1Mq1kq+doHHVQ=',
+          iotHubConnectionString: iotHubConnectionString
+        });
+        this.items.push({
+          deviceId: 'DevKit-Mock ' + new Date().toLocaleString(),
+          deviceConnectionString: 'HostName=azure-iot-toolkit-for-mobile.azure-devices.net;DeviceId=DevKit;SharedAccessKey=6lCf2leXhkD7N2pAUsGMx24LnaxfDJ9t5yMgzNmHutg=',
+          iotHubConnectionString: iotHubConnectionString
+        });
+        this.loadComplete(refresher);
+      }, 2000);
+    }
+  }
+
+  loadComplete(refresher = null) {
+    if (refresher) {
+      refresher.complete();
+    } else {
+      this.isLoading = false;
     }
   }
 
@@ -48,5 +63,9 @@ export class DeviceList {
     this.navCtrl.push(DevicePage, {
       item: item
     });
+  }
+
+  doRefresh(refresher) {
+    this.listDevices(refresher);
   }
 }
